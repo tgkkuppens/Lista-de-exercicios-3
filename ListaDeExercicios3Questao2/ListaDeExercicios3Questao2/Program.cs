18f46301691e1287352b86f743f6e89820bba3d5
Tiago Kuppens <tiago.garcia180@gmail.com>
2021-06-07 19:16:15 -0300

    Pronta

---
+++ ListaDeExercicios3Questao2/ListaDeExercicios3Questao2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeExercicios3Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string textoAnalisado = "...Uma atividade livre, conscientemente tomada como “não-séria” e exterior à vida habitual, mas ao mesmo tempo capaz de absorver o jogador de maneira intensa e total. É uma atividade desligada de todo e qualquer interesse material, com a qual não se pode obter qualquer lucro, praticada dentro de limites espaciais e temporais próprios, segundo uma certa ordem e certas regras. Promove a formação de grupos sociais com tendência a rodearem-se de segredo e a sublinharem sua diferença em relação ao resto do mundo por meio de disfarces ou outros meios semelhantes.";
+
+            Dictionary<string, int> resultado = VerificarQuantidadePalavrasDiferentes(textoAnalisado);
+            foreach (var item in resultado)
+            {
+                Console.WriteLine("A palavra '" + item.Key + "' se repete " + item.Value + " vez(es)");
+            }
+            Console.ReadLine();
+
+        }
+
+        static Dictionary<String, int> VerificarQuantidadePalavrasDiferentes(string texto)
+        {
+            Dictionary<String, int> palavrasColetadas = new Dictionary<string, int>();
+            string[] palavras = texto.Split(' ', ',', '.');
+            foreach (var item in palavras)
+            {
+                if (item.Length != 0)
+                {
+                    int count;
+                    if (!palavrasColetadas.TryGetValue(item, out count))
+                    {
+                        count = 0;
+                    }
+                    palavrasColetadas[item] = count + 1;
+                }
+            }
+
+            return palavrasColetadas;
+        }
+    }
+}
+
